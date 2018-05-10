a50cba0ea925c03b7064294bb0a74eba099f0167
Berlin Sky <pedroyoung8989@gmail.com>
2018-05-10 15:14:38 -0700

    add animation logic

--- pages/product.vue
+++ pages/product.vue
@@ -14,6 +14,32 @@
     transform-origin: 50% 50%;
   }
 
+  .page-enter-active {
+    animation: acrossIn .45s ease-out both;
+  }
+
+  .page-leave-active {
+    animation: acrossOut .65s ease-in both;
+  }
+
+  @keyframes acrossIn {
+    0% {
+      transform: translate3d(-100%, 0, 0);
+    }
+    100% {
+      transform: translate3d(0, 0, 0);
+    }
+  }
+
+  @keyframes acrossOut {
+    0% {
+      transform: translate3d(0, 0, 0);
+    }
+    100% {
+      transform: translate3d(100%, 0, 0);
+    }
+  }
+
   html, body {
     font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
     background: #222;
